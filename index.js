9f2c05f874b2b5be67375104cc497e2e68187db5
IEO360 <ibomhen.emmanuel@gmail.com>
2023-07-20 02:44:08 +0100

    started project

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+// initialise mongoose
+mongoose.connect(process.env.MONGO_URI);
+
+const con = mongoose.connection;
+con.on('open', error => {
+    if(!error){
+        console.log('DB connection successful');
+    }else{
+        console.log(`DB connection failed with error: ${error}`);
+    }
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//middleware
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}....`));
